584f743e2debce33572cde881463828c67d7f4c6
Glimound <2633956565@qq.com>
2023-12-15 17:06:53 +0800

    initial commit

---
+++ src/test/java/com/glimound/rmsbackend/RmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glimound.rmsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RmsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
